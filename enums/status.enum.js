935523fb23e9df81a2c41b3e82547f5a18afe17b
Arif Burak Demiray <burakdemiray09@hotmail.com>
2022-12-18 12:30:55 +0300

    chore: new statuses and their messages

--- enums/status.enum.js
+++ enums/status.enum.js
@@ -2,5 +2,7 @@ export const HttpStatus = {
     OK: 200,
     BAD_REQUEST: 400,
     TOO_MANY_REQUEST: 429,
-    ACCESS_DENIED: 403
+    ACCESS_DENIED: 403,
+    INTERNAL_SERVER_ERROR: 500,
+    UNAUTHORIZED: 401
 }
